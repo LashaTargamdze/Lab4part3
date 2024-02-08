5bb310a112c0bde1acf0633f2db64bd4fea4e6b2
Lasha <ltargama@andrew.cmu.edu>
2024-02-08 10:24:28 +0300

    commited

--- temp_conversion.rb
+++ temp_conversion.rb
@@ -1,8 +1,9 @@
-def convert(fahrenheit_temperature)
-    return "Temperature must be an integer" unless temp.class == Integer
-  (fahrenheit_temperature - 32) * 5 / 9
+def convert(temp)
+    return "Temperature must be an integer" if temp.class != Integer
+    return "Temperature below Absolute Zero" if temp < -474 
+  (temp - 32) * 5 / 9
 end
 puts convert(32)          
 puts convert(50)          
 puts convert(212)
-puts convert("zero")
+puts convert(-474)
